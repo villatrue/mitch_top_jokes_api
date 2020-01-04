5763ca63ed07f1f83c2510763f36a10d674753b3
villatrue <scarter0313@gmail.com>
2020-01-04 00:43:09 -0500

    postgresforreal

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_04_004856) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jokes", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
